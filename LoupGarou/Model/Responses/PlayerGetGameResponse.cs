1eb4426cccee76a1c897fd7e082252058b064f46
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-07-07 18:20:17 +0200

    add new requests dto for future use

---
+++ LoupGarou/Model/Responses/PlayerGetGameResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace LoupGarou.Model.Responses
+{
+    public class PlayerGetGameResponse
+    {
+        public Guid GameId { get; set; }
+        public string GameCode { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public string CurrentPhase { get; set; }
+        public string Status { get; set; }
+        public IList<PlayerGetPlayerResponse> Players { get; set; }
+        public IList<Role> Roles { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ LoupGarou/Model/Responses/PlayerGetPlayerResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace LoupGarou.Model.Responses
+{
+    public class PlayerGetPlayerResponse
+    {
+        public Guid PlayerId { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public Guid GameId { get; set; }
+    }
+}
